083d0229ab236f5ffc723ba704bb28324807975e
q0abd <qiyomovabdulloh3@gmail.com>
2023-03-04 09:23:11 +0500

    js added

---
+++ mock/categories.js
@@ -0,0 +1,26 @@
+const categories = [
+    {
+        title: "House Plants",
+        count: 33,
+    },
+    {
+        title: "Potter Plants",
+        count: 22,
+    },
+    {
+        title: "Seeds",
+        count: 12,
+    },
+    {
+        title: "Big Plants",
+        count: 21,
+    },
+    {
+        title: "Small Plants",
+        count: 99,
+    },
+    {
+        title: "Eng arzonlari",
+        count: 0,
+    },
+]
---
+++ mock/mockData.js
@@ -0,0 +1,81 @@
+const greenShopPlants = {
+    allPlants: [
+        {
+            img: "../assets/images/flower1.png",
+            title: "Barberton Daisy",
+            price: "$119.00"
+        },
+        {
+            img: "../assets/images/flower2.png",
+            title: "Atirgul zoridan",
+            price: "$229.00"
+        },
+        {
+            img: "../assets/images/flower3.png",
+            title: "Lola gul",
+            price: "$232.00"
+        },
+        {
+            img: "../assets/images/flower8.jpg",
+            title: "Ramashka",
+            price: "$742.00"
+        }, {
+            img: "../assets/images/flower1.png",
+            title: "Barberton Daisy",
+            price: "$119.00"
+        }
+    ],
+    newArrivals: [
+        {
+            img: "../assets/images/flower3.png",
+            title: "Barberton Daisy",
+            price: "$119.00"
+        },
+        {
+            img: "../assets/images/flower4.jpg",
+            title: "Atirgul zoridan",
+            price: "$229.00"
+        },
+        {
+            img: "../assets/images/flower7.jpg",
+            title: "Lola gul",
+            price: "$232.00"
+        },
+        {
+            img: "../assets/images/flower6.jpg",
+            title: "Ramashka",
+            price: "$742.00"
+        }, {
+            img: "../assets/images/flower9.jpg",
+            title: "Barberton Daisy",
+            price: "$119.00"
+        }
+    ],
+    sale: [
+        {
+            img: "../assets/images/flower7.jpg",
+            title: "Lola gul",
+            price: "$232.00"
+        },
+        {
+            img: "../assets/images/flower6.jpg",
+            title: "Ramashka",
+            price: "$742.00"
+        }, {
+            img: "../assets/images/flower9.jpg",
+            title: "Barberton Daisy",
+            price: "$119.00"
+        },
+        {
+            img: "../assets/images/flower3.png",
+            title: "Barberton Daisy",
+            price: "$119.00"
+        },
+        {
+            img: "../assets/images/flower4.jpg",
+            title: "Atirgul zoridan",
+            price: "$229.00"
+        },
+
+    ]
+}
